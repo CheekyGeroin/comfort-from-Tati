664a949be06b56f30dcaf7f75532710908119db0
CheekyGeroin <97744740+CheekyGeroin@users.noreply.github.com>
2022-11-11 23:17:12 +0000

    Deploying to gh-pages from @ CheekyGeroin/comfort-from-Tati@05a32becc030f131cb4371487d708c913736ca86 🚀

--- catalogList.ebe3182b.js
+++ catalogList.ebe3182b.js
@@ -1,2 +1,2 @@
 !function(){var e={openBtn:document.querySelector(".burger__btn"),menu:document.querySelector(".menu"),closeBtn:document.querySelector(".close__btn")},n=function(){e.menu.classList.remove("open"),e.closeBtn.removeEventListener("click",n)};({openBtn:document.querySelector(".burger__btn"),menu:document.querySelector(".menu"),closeBtn:document.querySelector(".close__btn")}).openBtn.addEventListener("click",(function(){e.menu.classList.add("open"),e.closeBtn.addEventListener("click",n)}))}();
-//# sourceMappingURL=index.3310e65d.js.map
+//# sourceMappingURL=catalogList.ebe3182b.js.map
