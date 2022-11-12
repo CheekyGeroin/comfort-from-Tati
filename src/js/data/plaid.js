e0e9dc386285201fe10e2678bab927e2e541d476
Danylo Altukhov <dan.97198@gmail.com>
2022-11-12 15:38:07 +0200

    upd second page

---
+++ src/js/data/plaid.js
@@ -0,0 +1,33 @@
+export const plaids = [
+  {
+    src: '',
+    smallImage: '',
+    smallImage2x: '',
+    mediumImage: '',
+    mediumImage2x: '',
+    largeImage: '',
+    largeImage2x: '',
+    title: 'Плед Плетінка',
+  },
+  {
+    src: '',
+    smallImage: '',
+    smallImage2x: '',
+    mediumImage: '',
+    mediumImage2x: '',
+    largeImage: '',
+    largeImage2x: '',
+    title: 'Плед Соти',
+  },
+  {
+    src: '',
+    smallImage: '',
+    smallImage2x: '',
+    mediumImage: '',
+    mediumImage2x: '',
+    largeImage: '',
+    largeImage2x: '',
+    title: 'Плед Серденько',
+  },
+];
+export const plaidsEntires = [{}, {}, {}];
