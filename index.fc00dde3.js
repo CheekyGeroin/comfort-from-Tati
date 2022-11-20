cb140941470a03c0aa11b5d9bb03b0a80681fe20
CheekyGeroin <97744740+CheekyGeroin@users.noreply.github.com>
2022-11-20 19:47:42 +0000

    Deploying to gh-pages from @ CheekyGeroin/comfort-from-Tati@8fbe245618d4289913fe6aada9ab94578106f2e1 🚀

--- index.fc00dde3.js
+++ index.fc00dde3.js
@@ -1,2 +1,2 @@
 const e={openBtn:document.querySelector(".burger__btn"),menu:document.querySelector(".menu"),closeBtn:document.querySelector(".close__btn")},n=()=>{e.menu.classList.remove("open"),e.closeBtn.removeEventListener("click",n)};({openBtn:document.querySelector(".burger__btn"),menu:document.querySelector(".menu"),closeBtn:document.querySelector(".close__btn")}).openBtn.addEventListener("click",(()=>{e.menu.classList.add("open"),e.closeBtn.addEventListener("click",n)}));
-//# sourceMappingURL=catalogList.7b6bf3f0.js.map
+//# sourceMappingURL=index.fc00dde3.js.map
