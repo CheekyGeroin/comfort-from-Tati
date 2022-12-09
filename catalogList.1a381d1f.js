9aa948ab248c6e49fc22a6050e6121b8a538eaa7
CheekyGeroin <97744740+CheekyGeroin@users.noreply.github.com>
2022-12-09 15:32:57 +0000

    Deploying to gh-pages from @ CheekyGeroin/comfort-from-Tati@e0d40895c0f7a826425ab9bdebf57aee396dc7c9 🚀

--- catalogList.1a381d1f.js
+++
@@ -1,2 +0,0 @@
-function t(t){if("BUTTON"!==t.target.nodeName)return;const e=document.querySelector(".switcher__buttons--active");e&&e.classList.remove("switcher__buttons--active"),t.target.classList.add("switcher__buttons--active")}({list:document.querySelector(".switcher__list")}).list.addEventListener("click",t);var e=t;({list:document.querySelector(".switcher__list")}).list.addEventListener("click",e);
-//# sourceMappingURL=catalogList.1a381d1f.js.map
--- catalogList.42774ad4.js
+++
@@ -1,2 +0,0 @@
-!function(){function t(t){if("BUTTON"===t.target.nodeName){var e=document.querySelector(".switcher__buttons--active");e&&e.classList.remove("switcher__buttons--active"),t.target.classList.add("switcher__buttons--active")}}({list:document.querySelector(".switcher__list")}).list.addEventListener("click",t);var e=t;({list:document.querySelector(".switcher__list")}).list.addEventListener("click",e)}();
-//# sourceMappingURL=catalogList.42774ad4.js.map
